6915fde94a2eb1f553459d6762ce16635704c614
sheetalMehta7 <sheetalmehta137@gmail.com>
2024-09-11 19:56:30 +0530

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-3xl text-gray-300 font-semibold text-center mt-6">
+        Time to get started!
+      </h1>
+    </main>
+  );
+}
